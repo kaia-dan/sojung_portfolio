92d3cbd95037846bb164cc0923ec8db7db8956b6
kaia <111490006+kaia-dan@users.noreply.github.com>
2022-12-19 17:28:47 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -74,3 +74,53 @@ cursorModifiers.forEach((cursorModifier) => {
     cursor.classList.remove(attribute);
   });
 });
+
+// 마우스스크롤부드럽게
+// class Scrooth {
+//   constructor({element = window, strength=10, acceleration = 1.2,deceleration = 0.975}={}) {
+//     this.element = element;
+//     this.distance = strength;
+//     this.acceleration = acceleration;
+//     this.deceleration = deceleration;
+//     this.running = false;
+
+//     this.element.addEventListener('wheel', this.scrollHandler.bind(this), {passive: false});
+//     this.element.addEventListener('mousewheel', this.scrollHandler.bind(this), {passive: false});
+//     this.scroll = this.scroll.bind(this);
+//   }
+
+//   scrollHandler(e) {
+//     e.preventDefault();
+
+//     if (!this.running) {
+//       this.top = this.element.pageYOffset || this.element.scrollTop || 0;
+//       this.running = true;
+//       this.currentDistance = e.deltaY > 0 ? 0.1 : -0.1;
+//       this.isDistanceAsc = true;
+//       this.scroll();
+//     } else {
+//       this.isDistanceAsc = false;
+//       this.currentDistance = e.deltaY > 0 ? this.distance : -this.distance;
+//     }
+//   }
+
+//   scroll() {
+//     if (this.running) {
+//       this.currentDistance *= this.isDistanceAsc === true ? this.acceleration : this.deceleration;
+//       Math.abs(this.currentDistance) < 0.1 && this.isDistanceAsc === false ? this.running = false : 1;
+//       Math.abs(this.currentDistance) >= Math.abs(this.distance) ? this.isDistanceAsc = false : 1;
+
+//       this.top += this.currentDistance;
+//       this.element.scrollTo(0, this.top);
+      
+//       requestAnimationFrame(this.scroll);
+//     }
+//   }
+// }
+
+// const scroll = new Scrooth({
+//   element: window,
+//   strength: 20,
+//   acceleration: 1.5,
+//   deceleration: 0.975,
+// });
